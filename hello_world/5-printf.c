f83628adf45053c40b320c0a9666fb77a34db3cb
luciel53 <l.leroty@gmail.com>
2022-06-06 17:59:07 +0200

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art\b,");
+printf("with proper grammar\b, but the outcome is a piece of art\b,");
 return (0);
 }
