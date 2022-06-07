7faecd09e3c670c3fd12860d4d69345f5714a5d1
luciel53 <l.leroty@gmail.com>
2022-06-07 14:20:57 +0200

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ if (n % 10 > 5)
 puts("and is greater than 5");
 else if (n % 10 == 0)
 puts("and is 0");
-else if (n % 10 < 6, n != 0)
+else if (n % 10 < 6 != 0)
 puts("and less than 6 and not 0");
 	return (0);
 }
