baf5603b1f916b60a2fa0d6a2f50d9b0e6925908
luciel53 <l.leroty@gmail.com>
2022-06-07 12:50:10 +0200

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 if (n > 0)
-puts("is positive");
+puts("$n is positive");
 else
 if (n == 0)
-puts("is zero");
+puts("$n is zero");
 else
 if (n < 0)
-puts("is negative");
+puts("$n is negative");
 
 	return (0);
 }
