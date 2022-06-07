f6ebdaec0c84405c0277d348f7ccd79bb4b12a9c
luciel53 <l.leroty@gmail.com>
2022-06-07 23:32:34 +0200

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,14 @@
 int main(void)
 {
 char ch;
-for (ch = 'a' ; ch < 'z' ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 putchar(ch);
 if ((ch == 'd') || (ch == 'p'))
 {ch++; }
+{
+putchar('\n');
+}
 }
 return (0);
 }
