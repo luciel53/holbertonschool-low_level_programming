657c473921751fcd0547055734196aec6dfca715
luciel53 <l.leroty@gmail.com>
2022-06-08 10:34:58 +0200

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 int num;
 for (num = '0' ; num <= '9' ; num++)
 {
-putchar((num % 10) + '0');
+putchar((num) + '0');
 if (num == 0)
 continue;
-putchar(',');
+putchar(', ');
 }
 putchar('\n');
 return (0);
