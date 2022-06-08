72236a3cfb5a3431946560f875374fa459c88845
luciel53 <l.leroty@gmail.com>
2022-06-08 22:21:25 +0200

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Entry point
+* int _islower(int c): lower case
+* Return: Always 0
+*/
+int main(void)
+{
+int r;
+int _islower(int c);
+{
+r = _islower('H');
+_putchar(r + '0');
+r = _islower('o');
+_putchar(r + '0');
+r = _islower(108);
+_putchar(r + '0');
+_putchar('\n');
+}
+return (0);
+}
