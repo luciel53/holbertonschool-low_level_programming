1fe9d4665d2cf8834b367d29930e7d55adeb8bfa
luciel53 <l.leroty@gmail.com>
2022-06-09 14:05:35 +0200

    6-abs

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -20,7 +20,7 @@ else
 else
 	if (n == '0')
 	{
-		_putchar(0);
+		_putchar(45);
 		return (0);
 	}
 return (0);
---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* int _abs(int)
+*
+* Return: Always 0
+*/
+int _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+return (n);
+}
