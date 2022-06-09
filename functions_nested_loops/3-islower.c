9db7d5e85218b9950b432eb2a69fb4b55d35e6b1
luciel53 <l.leroty@gmail.com>
2022-06-09 11:05:38 +0200

    4-isalpha.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* _islower(int c) - the _islower function
+* int _islower(int c) - the _islower function
 *
 * Return: Always 0
 */
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
-* 
+* int _isalpha(int c): 'check lower and upper cases'
 *
 * Return: Always 0
 */
 int _isalpha(int c)
 {
-int r;
-
-r = _isalpha('H');
-_putchar(r + '0');
-r = _isalpha('o');
-_putchar(r + '0');
-r = _isalpha(108);
-_putchar(r + '0');
-r = _isalpha(';');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		if (c >= 'a' && c <= 'z')
+		{
+			return (1);
+		}
+	else
+		return (0);
 }
