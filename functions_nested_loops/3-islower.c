6739a47c6702a22aba07dbe2a3c6a174db64f454
luciel53 <l.leroty@gmail.com>
2022-06-09 10:38:31 +0200

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,18 +1,13 @@
 #include "main.h"
 /**
-* _islower - the _islower function
-* int r
+* _islower(int c) - the _islower function
+*
 * Return: Always 0
 */
 int _islower(int c)
 {
-
-c = _islower('H');
-_putchar(c + '0');
-c = _islower('o');
-_putchar(c + '0');
-c = _islower(108);
-_putchar(c + '0');
-_putchar('\n');
-return (0);
+	if(c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
