6cc60668217824fe89d59b6c691f0c2f750441ae
luciel53 <l.leroty@gmail.com>
2022-06-12 18:20:26 +0200

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 
 void print_to_98(int n)
 {
-	for (n = 0 ; n <= 98 ; n++)
+	for (n = 0 ; n < 99 ; n++)
 	{
 		if (n < 10)
 		{
@@ -17,14 +17,28 @@ void print_to_98(int n)
 			_putchar (',');
 			_putchar (' ');
 		}
-
 		if (n > 10)
-		{
+		{		
 			_putchar (n / 10 + '0');
 			_putchar (n % 10 + '0');
 			_putchar (',');
 			_putchar (' ');
 		}
+	}
+	for (n = 0 ; n > 97 ; n--)
+	{
+		_putchar (n / 10 + '0');
+		_putchar (n % 10 + '0');
+		_putchar (',');
+		_putchar (' ');
+	}
+	if ( n == 98)
+	{
+		
+		_putchar (n / 10 + '0');
+		_putchar (n % 10 + '0');
+		_putchar (',');
+		_putchar (' ');
 	}
 _putchar (10);
 }
