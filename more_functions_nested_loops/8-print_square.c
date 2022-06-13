aedc3175c06964d56167f42cea4b9f5b0c7084c8
luciel53 <l.leroty@gmail.com>
2022-06-14 00:59:02 +0200

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* print_square - function
+* @size: operator
+* Return: void
+*/
+void print_square(int size)
+{
+int i;
+int x;
+
+if (size <= 0)
+{
+	_putchar (10);
+}
+else
+{
+	for (x = 0 ; x < size ; x++)
+	{
+		for (i = 1 ; i <= size ; i++)
+		{
+			_putchar ('#');
+		}
+	_putchar ('#');
+	_putchar (10);
+	}
+}
+}
