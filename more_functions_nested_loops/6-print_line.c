a20374cd91a58731824fdf01a5d68f48e7447357
luciel53 <l.leroty@gmail.com>
2022-06-13 21:45:49 +0200

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_line - function
+* @n: operator
+* Return: void
+*/
+
+void print_line(int n)
+{
+int c = 0;
+
+	for (c = 0 ; c < n ; c++)
+	{
+		_putchar ('_');
+	}
+	_putchar (10);
+}
