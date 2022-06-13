0299e6cbc77198c1df775d82a846cd2e1e53eecb
luciel53 <l.leroty@gmail.com>
2022-06-13 12:02:51 +0200

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - function
+* @c: operator
+* Return: 1 if c is a digit 0 if otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
