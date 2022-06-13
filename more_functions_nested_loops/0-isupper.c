df3e9f8581b856390f19ce4adc38fe535bcb53ff
luciel53 <l.leroty@gmail.com>
2022-06-13 11:37:04 +0200

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isupper - function
+* @c: operator
+* Return: 1 if uppercase and 0 if otherwise
+*/
+
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+			return (0);
+	}
+}
