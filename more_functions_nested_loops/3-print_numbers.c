902de365f8f49e92d3bd11d6ef7fa9eb768f948d
luciel53 <l.leroty@gmail.com>
2022-06-13 13:23:44 +0200

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - function
+*
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int x = 0;
+		for (x = '0' ; x <= '9' ; x++)
+		{
+			_putchar (x);
+		}
+_putchar (10);
+}
