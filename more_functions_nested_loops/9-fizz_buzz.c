bbbfdc60462b6312df0de536faad36b8eed49759
luciel53 <l.leroty@gmail.com>
2022-06-14 13:27:13 +0200

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - function
+*
+* Return: void
+*/
+
+int main(void)
+{
+int i = 1;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 3 == 0)
+		{
+			_putchar ('F');
+			_putchar ('i');
+			_putchar ('z');
+			_putchar ('z');
+		}
+		if (i % 5 == 0)
+		{
+			_putchar ('B');
+			_putchar ('u');
+			_putchar ('z');
+			_putchar ('z');
+		}
+				if (i >= 10)
+				{
+					_putchar (i / 10 + '0');
+				}
+	_putchar (i % 10 + '0');
+	_putchar (' ');
+	}
+_putchar (10);
+return (1);
+}
