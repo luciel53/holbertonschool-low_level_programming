0769c7af528a17b14fb4fe8b143b7afa735c4e4d
luciel53 <l.leroty@gmail.com>
2022-06-15 23:55:25 +0200

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* puts2 - function
+* @str: operator
+* Return: void
+*/
+
+void puts2(char *str)
+{
+int i = 0;
+int counter = 0;
+		
+		while (str[i])
+		{
+			counter++;
+			i++;
+		}
+		for (i = 0 ; i < counter ; i++)
+		{
+			if (i % 2 == 0)
+			{
+				_putchar (str[i]);
+			}
+		}
+		_putchar ('\n');
+}
