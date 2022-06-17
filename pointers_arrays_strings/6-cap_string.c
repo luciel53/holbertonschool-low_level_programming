652007d7baf46d1dae20a989514e02eb8a7ccc15
luciel53 <l.leroty@gmail.com>
2022-06-17 12:24:18 +0200

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* cap_string - function
+* @p: operator
+
+* Return: p
+*/
+
+char *cap_string(char *p)
+{
+
+int i = 0;
+
+	while (p[i] != '\0')
+	{
+		i++;
+		if (i == 0)
+		{
+			if (p[i] >= 'a' && p[i] <= 'z')
+				p[i] = p[i] - 32;
+			continue;
+		}
+		if (p[i] == ' ' || p[i] == ',' || p[i] == ';' || p[i] == '.' || p[i] == '!' || p[i] == '?' || p[i] == '"' || p[i] == '(' || p[i] == ')' || p[i] == '{' || p[i] == '}')
+		{
+			i++;
+				if (p[i] >= 'a' && p[i] <= 'z')
+				{
+					p[i] = p[i] - 32;
+					continue;
+				}
+		}
+	}
+return (p);
+}
