09369c2060739c3cc4b049ae0d402c7f2420fc01
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 <l.leroty@gmail.com>
2022-06-23 10:12:47 +0200

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -12,12 +13,20 @@ int main(int argc, char *argv[])
 {
 int num1 = 0;
 int num2 = 0;
+int result = num1 * num2;
 
-	(void) argv;
-	printf("%d\n", num1 * num2);
-	if (argc < 2)
+	if (argc == 3)
 	{
-		printf("Error\n");
+		result += atoi(*argv++);
+		num1 += atoi(argv[1]);
+		num2 += atoi(argv[2]);
+		printf("%d\n", result);
+		putchar (10);
+	}
+	else
+	{
+		printf("Error");
+		putchar (10);
 		return (1);
 	}
 	return (0);
