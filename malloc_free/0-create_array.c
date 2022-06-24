38b0a5a446406fdec7f5f8fb7c99eaf1eb31cad2
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 <l.leroty@gmail.com>
2022-06-24 20:12:45 +0200

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,6 +14,11 @@ char *create_array(unsigned int size, char c)
 unsigned int i;
 char *str;
 	str = malloc(size * sizeof(char));
+		if (str == NULL)
+		{
+			free(str);
+			return (NULL);
+		}
 	if (size > 0)
 		for (i = 0 ; i < size ; i++)
 		{
