4c7ba42923f9254558f4e2df337702346a831247
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 <l.leroty@gmail.com>
2022-06-24 22:12:52 +0200

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,14 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-int num1 = 0;
-int num2 = 0;
+int num1;
+int num2;
+int result;
 
 	if (argc == 3)
 	{
-		num1 = atoi(*argv);
-		num2 = atoi(*argv);
-		printf("%d\n", num1 * num2);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
 	}
 	else
 	{
