4071aa54c93d6341592c65483a34879c96b2362d
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 <l.leroty@gmail.com>
2022-06-24 19:09:08 +0200

    3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* alloc_grid - function
+* @width: operator and pointer
+* @height: operator and pointer
+* Return: s
+*/
+
+int **alloc_grid(int width, int height)
+{
+
+int **a;
+int i;
+int j;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	a = malloc(sizeof(int *) * height);
+
+	if (a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+
+	for (j = 0 ; j < height ; j++)
+	{
+		a[j] = malloc(sizeof(int *) * width);
+
+			if (a[j] == NULL)
+			{
+				for (; j < height ; j++)
+				{
+					free(a);
+					return (NULL);
+				}
+			}
+	}
+
+	for (j = 0 ; j < height ; j++)
+	{
+		for (i = 0 ; i < height ; i++)
+			a[i][j] = 0;
+	}
+return (a);
+}
