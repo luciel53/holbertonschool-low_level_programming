186c99d8efe9b2d02064a190ce48501de20e2ed9
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 <l.leroty@gmail.com>
2022-06-24 21:26:57 +0200

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - function
+* @argc: argument
+* @argv: vector argument
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+int j;
+int sum;
+
+	if (argc > 1)
+	{
+		for (i = 0 ; i < argc ; i++)
+		{
+			for (j = 0 ; j < argv[i][j] ; j++)
+			{
+				sum = atoi(argv[i]);
+				printf("%d\n", sum);
+			}
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+return (0);
+}
