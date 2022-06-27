c308fd7735eb21214347c736cf8eeeb95df5543a
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 <l.leroty@gmail.com>
2022-06-27 23:26:07 +0200

    3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,11 +13,13 @@ int *array_range(int min, int max)
 {
 int *a;
 int i = 0;
+int size = 0;
 
 	if (min > max)
 		return (NULL);
 
-	a = malloc(sizeof(int) * (max - min) + sizeof(int));
+	size = max - min +1;
+	a = malloc(sizeof(int) * size);
 
 	if (!a)
 		return (NULL);
