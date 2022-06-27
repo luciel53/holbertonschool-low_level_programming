0d50c18752cbffc9cfe6b856d2500850db3ae2a8
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 <l.leroty@gmail.com>
2022-06-27 23:17:17 +0200

    3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* array_range - function
+* @min: operator
+* @max: operator
+* Return: a
+*/
+
+int *array_range(int min, int max)
+{
+int size;
+int *a;
+int i = 0;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+	a = malloc(sizeof(int) * size);
+
+	if (!a)
+		return (NULL);
+
+	while (i <= max)
+	{
+		a[i] = min;
+		i++;
+		min++;
+	}
+return (a);
+}
