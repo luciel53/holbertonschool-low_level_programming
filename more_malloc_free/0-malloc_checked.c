5be1e9a563225d240588e1f6e52b0dd8a095dbe1
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 <l.leroty@gmail.com>
2022-06-27 12:11:35 +0200

    0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* malloc_checked - function
+* @b: operator
+* Return: void
+*/
+
+void *malloc_checked(unsigned int b)
+{
+
+void *p;
+
+	p = malloc(sizeof(int) * b);
+
+	if (!b)
+	exit(98);
+	return (p);
+}
