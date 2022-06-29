1f44574ec60fc5207e4841a0f24aa91b1ec0615e
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 <l.leroty@gmail.com>
2022-06-29 17:38:26 +0200

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,9 @@ struct dog
 	float age;
 };
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t mydog(char *name, float age, char *owner);
+void _free(dog_t *d);
 #endif
