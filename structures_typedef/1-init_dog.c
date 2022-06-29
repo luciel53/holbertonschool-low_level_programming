166161ccf2016b6e04f1c3541138561a5e43e9cb
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 <l.leroty@gmail.com>
2022-06-29 13:58:35 +0200

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog -function
+ * @name: pointer and operator
+ * @age: operator
+ * @owner: pointer and operator
+ * @d: pointer
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+			d->name = name;
+			d->age = age;
+			d->owner = owner;
+}
