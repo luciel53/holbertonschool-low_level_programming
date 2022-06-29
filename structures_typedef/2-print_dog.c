a4b2a354c7f790644b03f0b58734922f4ee38e31
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 <l.leroty@gmail.com>
2022-06-29 16:31:05 +0200

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,6 +12,8 @@ void print_dog(struct dog *d)
 {
 	struct dog;
 
+	if (d == NULL)
+		return;
 	if (d->name == NULL)
 		d->name = "Name: (nil)";
 	if (d->owner == NULL)
@@ -19,7 +21,5 @@ void print_dog(struct dog *d)
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
-	if (d == NULL)
-		return;
 
 }
