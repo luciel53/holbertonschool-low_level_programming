cfb7b91b299a4be3cca65e6a19a020c331d6de8b
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 <l.leroty@gmail.com>
2022-06-29 21:52:19 +0200

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,4 +21,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t mydog(char *name, float age, char *owner);
 void _free(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
