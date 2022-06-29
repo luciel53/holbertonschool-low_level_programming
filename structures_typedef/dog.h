9287f8511f41ac0553e8e415cd63588fc8762a56
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 <l.leroty@gmail.com>
2022-06-29 12:40:34 +0200

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structure
+* @name: operator and pointer
+* @owner: operator and pointer
+* @age: operator
+* description: define new type struct dog
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
