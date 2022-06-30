29a0e7b922bc01594ec878405c774951af8e3e0f
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 <l.leroty@gmail.com>
2022-06-30 14:39:11 +0200

    2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,16 +14,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	if (array != NULL || cmp != NULL)
+	if (size > 0)
 	{
-		for (i = 0 ; i < size ; i++)
+		if (array != NULL || cmp != NULL)
 		{
-			if ((cmp(array[i])))
-				return (i);
+			for (i = 0 ; i < size ; i++)
+			{
+				if ((cmp(array[i])))
+					return (i);
+			}
+		return (-1);
 		}
-	return (-1);
 	}
-return (0);
+	return (-1);
 }
+
