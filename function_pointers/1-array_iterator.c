4a10573632d078d23b14272a8682cb32b8ea033d
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 <l.leroty@gmail.com>
2022-06-30 12:17:50 +0200

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function pointer
+ * @array: pointer and operator
+ * @size: operator
+ * @action: pointer
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+int j = size;
+	for (i = 0 ; i < j ; i++)
+	{
+		if (action != NULL)
+		action(array[i]);
+	}
+}
