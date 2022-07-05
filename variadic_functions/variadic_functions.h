27d5fca4252fae454075e5649baa902b99ad8dbb
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 <l.leroty@gmail.com>
2022-07-05 19:51:43 +0200

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 
 int _putchar(char c);
@@ -25,5 +28,16 @@ char *_strstr(char *haystack, char *needle);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * structure
+ * @f: pointer
+ */
+typedef struct format
+{
+char *p;
+void(*f)(va_list);
+} f_t;
 
 #endif
