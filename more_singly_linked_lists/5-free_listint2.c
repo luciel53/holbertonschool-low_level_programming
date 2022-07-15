aba854668cff1bf987bf41d9999f7842dd6adab2
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 <l.leroty@gmail.com>
2022-07-15 21:05:37 +0200

    adding 5-free_listint2.c

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a listint_t list.
+ * @head: double pointer head to head node
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+		listint_t *temp;
+
+
+		while (*head != NULL)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
+}
