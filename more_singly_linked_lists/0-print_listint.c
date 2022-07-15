eef70c30dcb850c8f037f46e179b5d3713c47cec
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 <l.leroty@gmail.com>
2022-07-15 14:59:12 +0200

    add 0-print_listint.c

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -12,10 +12,11 @@ size_t print_listint(const listint_t *h)
 {
 		int i = 0;
 
-		while (h != NULL)
+		while (h)
 		{
-			h = h->next;
+			printf("%d\n", h->n);
 			i++;
+			h = h->next;
 		}
 
 return (i);
