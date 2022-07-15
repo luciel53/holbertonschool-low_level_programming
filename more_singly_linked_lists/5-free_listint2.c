f3c672581bb3c148fbdbdf458b0d54cc1687a189
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 <l.leroty@gmail.com>
2022-07-15 21:19:56 +0200

    adding 5-free_listint2.c

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -17,8 +17,8 @@ void free_listint2(listint_t **head)
 
 		while (*head != NULL)
 		{
-			temp = *head;
-			*head = temp->next;
-			free(temp);
+			temp = (*head)->next;
+			free(*head);
+			*head = temp;
 		}
 }
