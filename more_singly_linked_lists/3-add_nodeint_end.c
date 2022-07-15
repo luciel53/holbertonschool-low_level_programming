66f6e336682992fc444bf9367a88ea9e0b77a81e
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 <l.leroty@gmail.com>
2022-07-15 20:37:56 +0200

    add 3-add_nodeint_end.c

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,6 +17,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		/*allocate new node*/
 		new_node = malloc(sizeof(listint_t));
 
+		if (new_node == NULL)
+			return (NULL);
+
 		new_node->n = n1;
 		new_node->next = NULL;
 
