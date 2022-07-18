f1e494da6cf202e69621558046cdaf69328acf43
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 <l.leroty@gmail.com>
2022-07-18 21:59:59 +0200

    update 1-print_binary

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -10,9 +10,7 @@
 
 void print_binary(unsigned long int n)
 {
-	int i = n % 2;
-		if (n > 1)
-			print_binary(n / 2);
-
-		_putchar(i + '0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar ((n & 1) + '0');
 }
