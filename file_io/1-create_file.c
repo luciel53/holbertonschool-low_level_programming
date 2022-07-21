fe998874176e402fe081074ce9f8bc6368df8b60
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 <l.leroty@gmail.com>
2022-07-21 15:45:30 +0200

    add 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
 
 	if (fd < 0)
 		return (-1);
