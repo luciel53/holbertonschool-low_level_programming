2b5a8381ceb185dc93130e69ba101f1edd9ea299
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 <l.leroty@gmail.com>
2022-07-21 16:05:27 +0200

    add 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[size] != '\0')
+		while (text_content[size])
 		{
 			size++;
 		}
