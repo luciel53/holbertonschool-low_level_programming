c7f91d2bfd6b463b64316f12e426b3c40b2ed0c6
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 <l.leroty@gmail.com>
2022-07-22 20:21:27 +0200

    add 3-add_dnodeint_end.c

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * *add_dnodeint_end - function that adds new node at the beginning of a list.
+ * @head: double pointer to head
+ * @n: data
+ * Return: number of nodes
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *last_node;
+
+	/* allocate node*/
+	new_node = malloc(sizeof(dlistint_t));
+
+	last_node = *head;
+
+	/* give data to the new node*/
+	new_node->n = n;
+
+	/* new node will be the last node */
+	new_node->next = NULL;
+
+	/* if list empty, new node is head */
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* else, go to the last node */
+	while (last_node->next != NULL)
+	{
+		last_node = last_node->next;
+	}
+
+	/* change the next of last node */
+	last_node->next = new_node;
+
+	/* make last node as previous of new node */
+	new_node->prev = last_node;
+
+	return (new_node);
+
+}
