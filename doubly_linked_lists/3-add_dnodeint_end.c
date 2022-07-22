6b4a61577af74aed1f976b8d1b0a33e4ae7db8ea
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 <l.leroty@gmail.com>
2022-07-22 20:23:12 +0200

    add 3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,6 +16,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	/* allocate node*/
 	new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	last_node = *head;
 
 	/* give data to the new node*/
