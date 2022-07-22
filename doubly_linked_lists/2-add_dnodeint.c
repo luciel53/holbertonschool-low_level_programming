e6361ebc95688acb55aa81894c54d03fb7ab740b
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 <l.leroty@gmail.com>
2022-07-22 18:44:05 +0200

    update 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,6 +15,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = n;
 
 	new_node->next = *head;
@@ -27,4 +30,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	*head = new_node;
 
 	return (new_node);
+
 }
