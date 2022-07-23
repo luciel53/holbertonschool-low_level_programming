2526c60cca03a3b710fb62c04fc074150f916ca3
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 <l.leroty@gmail.com>
2022-07-23 17:40:19 +0200

    add 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (idx != 0)
+	while (idx != 1)
 	{
 		temp = temp->next;
 		idx--;
