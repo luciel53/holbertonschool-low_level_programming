9fb591a71a9e1fb0c583f071a8f90359ced659de
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 <l.leroty@gmail.com>
2022-08-19 13:19:02 +0200

    add 0 task

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,23 +10,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
 	hash_node_t **ar;
-	unsigned long int i;
 
 	new_ht = malloc(sizeof(hash_table_t));
 
 	if (new_ht == NULL)
 		return (NULL);
 
-	ar = malloc(sizeof(hash_node_t) * size + 1);
+	ar = malloc(sizeof(hash_node_t *) * size);
 
 	if (ar == NULL)
 		return (NULL);
 
-	for (i = 0 ; i < size ; i++)
-	{
 	new_ht->size = size;
 	new_ht->array = ar;
-	}
+
 
 	return (new_ht);
 }
