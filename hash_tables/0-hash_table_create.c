53f58de3ffada25397c8547e170f1c97b3fba687
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 <l.leroty@gmail.com>
2022-08-19 10:59:21 +0200

    add 0 task

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,10 +10,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
 
-	new_ht = malloc(sizeof(size));
+	new_ht = malloc(sizeof(hash_table_t));
 
 	if (new_ht == NULL)
 		return (NULL);
+
+	new_ht->size = size;
 
 	return (new_ht);
 }
