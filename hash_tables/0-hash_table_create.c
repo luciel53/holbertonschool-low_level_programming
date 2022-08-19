5bf3d148fdfb39e7eb7d348acd6f883ac8541c4e
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 <l.leroty@gmail.com>
2022-08-19 11:22:59 +0200

    add 0 task

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#define CAPACITY 50000
 
 /**
  * hash_table_create - A function that create a new hash table
@@ -10,12 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
 
-	new_ht = malloc(sizeof(2048));
+	new_ht = malloc(sizeof(hash_table_t));
 
 	if (new_ht == NULL)
 		return (NULL);
 
 	new_ht->size = size;
+
 
 	return (new_ht);
 }
