48fd443c993753476d3dddda3f2dfbeee018db9c
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 <l.leroty@gmail.com>
2022-08-20 23:14:43 +0200

    add task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,18 +9,20 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index, i;
+	hash_node_t *temp;
 
 	index = key_index((unsigned char *) key, ht->size);
+
+	temp = ht->array[index];
 
 	if (!ht)
 		return (NULL);
 
 	for (i = index ; ht->array[i] ; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-			return (ht->array[i]->value);
-		ht->array[i] = ht->array[i]->next;
-	}
-
+		if (strcmp(ht->array[i]->key, temp->key) == 0)
+		{
+			return (temp->value);
+			temp = temp->next;
+		}
 	return (NULL);
 }
