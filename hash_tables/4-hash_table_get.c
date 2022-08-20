960c725b1caeedb157ddd58f785a089156c33089
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 <l.leroty@gmail.com>
2022-08-20 22:49:08 +0200

    4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,16 +9,14 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index, i;
-	hash_node_t *temp;
 
 	index = key_index((unsigned char *) key, ht->size);
-	temp = ht->array[index];
 
 	if (!ht)
 		return (NULL);
 
 	for (i = index ; ht->array[i] ; i++)
 		if (strcmp(ht->array[i]->key, key) == 0)
-			return (temp->value);
+			return (ht->array[i]->value);
 	return (NULL);
 }
