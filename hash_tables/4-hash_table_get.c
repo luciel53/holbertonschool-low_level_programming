2a7efe4e2825acac497771952b4f130950bb05ad
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 <l.leroty@gmail.com>
2022-08-20 23:44:55 +0200

    add task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -19,10 +19,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	while (temp)
+	{
 		if (strcmp(temp->key, key) == 0)
 		{
 			return (temp->value);
 		temp = temp->next;
 		}
+	}
 	return (NULL);
 }
