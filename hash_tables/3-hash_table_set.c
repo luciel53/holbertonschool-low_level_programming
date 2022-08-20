699939cdba3047901dda1a6594f5876ce7dd2e2f
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 <l.leroty@gmail.com>
2022-08-20 21:23:56 +0200

    add task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,34 +14,40 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_element;
 	char *value_copy;
 
+	/* if ht, key and *key are empty, return 0' */
 	if (!ht || !key || *key == '\0')
 		return (0);
 
+	/* make a copy of value */
 	value_copy = strdup(value);
 
+	/* if value_copy is NULL, return 1 */
 	if (value_copy == NULL)
 		return (1);
 
+	/* function that gives you the index of a key. */
 	index = key_index((unsigned char *) key, ht->size);
 
 	for (i = index ; ht->array[i] ; i++)
 	{
+		/* compares the key with the key of the array */
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
+			/* store the value in value_copy */
 			value_copy = ht->array[i]->value;
 			return (1);
 		}
 	}
+	/*create and allocate new element */
 	new_element = malloc(sizeof(hash_table_t));
 
 	if (new_element == NULL)
 		return (0);
 
+	/*copy the key in the new_element */
 	new_element->key = strdup(key);
 	new_element->value = value_copy;
 	new_element->next = ht->array[index];
 	ht->array[index] = new_element;
-
-
 	return (1);
 }
