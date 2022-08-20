fbd3633ea6484f4183041a5efdbe315ab536a67f
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 <l.leroty@gmail.com>
2022-08-20 22:18:51 +0200

    4-hash_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - A function that retrieves a value associated with a key.
+ * @ht: the hash table you want to look into
+ * @key: the key you are looking for
+ * Return: the value you search
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+	hash_node_t *temp;
+
+	index = key_index((unsigned char *) key, ht->size);
+	temp = ht->array[index];
+
+	if (!ht)
+		return (NULL);
+
+	for (i = index ; ht->array[i] ; i++)
+		if (ht != NULL && key != NULL)
+			return (temp->value);
+	return (NULL);
+}
