e6fdfc0308fc6f7bb693650a0ecca7bca60cd6a2
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 <l.leroty@gmail.com>
2022-08-21 10:47:40 +0200

    add task 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - A function that prints a hash table.
+ * @ht: the hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (i = 0 ; i < ht->size ; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			printf(","),
+			ht->array[i] = ht->array[i]->next;
+		}
+	}
+
+	printf("}\n");
+
+}
